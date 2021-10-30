cb20b5d2205bbda7e8b48bb1f3893c1fdbef74f9
DESKTOP-CQH0EH6\ghdco <ghdcoalss33@gmail.com>
2021-10-30 20:44:58 +0900

    [minHong][minHong] 코드로 배우는 스프링 Part1 Chapter 4

--- jex00/src/main/java/org/zerock/config/RootConfig.java
+++ jex00/src/main/java/org/zerock/config/RootConfig.java
@@ -2,6 +2,9 @@ package org.zerock.config;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -11,13 +14,16 @@ import com.zaxxer.hikari.HikariDataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"org.zerock.sample"})
+@MapperScan(basePackages = {"org.zerock.mapper"})
 public class RootConfig {
 
 	@Bean
 	public DataSource dataSource() {
 		HikariConfig hikariConfig = new HikariConfig();
-		hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
-		hikariConfig.setJdbcUrl("jdbc:cracle:thin:@localhost:1521:XE");
+		// hikariConfig.setDriverClassName("oracle.jdbc.driver.OracleDriver");
+		// hikariConfig.setJdbcUrl("jdbc:oracle:thin:@localhost:1521:XE");
+		hikariConfig.setDriverClassName("net.sf.log4jdbc.sql.jdbcapi.DriverSpy");
+		hikariConfig.setJdbcUrl("jdbc:log4jdbc:oracle:thin:@localhost:1521:XE");
 		hikariConfig.setUsername("c##book_ex");
 		hikariConfig.setPassword("c##book_ex");
 		
@@ -25,6 +31,13 @@ public class RootConfig {
 		
 		return dataSource;
 		
+	}
+	
+	@Bean
+	public SqlSessionFactory sqlSessionFactory() throws Exception {
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(dataSource());
+		return (SqlSessionFactory) sqlSessionFactory.getObject();
 	}
 	
 }
