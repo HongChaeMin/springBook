cc2fad407ef2eb2deafe0f5446c21f26d9d042e9
DESKTOP-CQH0EH6\ghdco <ghdcoalss33@gmail.com>
2021-10-31 22:54:50 +0900

    [minHong] 코드로 배우는 스프링 Part3 Chapter 10

---
+++ ex02/src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,66 @@
+package org.zerock.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.domain.BoardVO;
+import org.zerock.service.BoardService;
+
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/board/*")
+public class BoardController {
+
+	@Autowired
+	private BoardService boardService;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		log.info("list");
+		model.addAttribute("list", boardService.selectBoardList());
+	}
+	
+	@PostMapping("/insertBoard")
+	public String insertBoard(BoardVO board, RedirectAttributes rttr) {
+		log.info("insert : " + board);
+		
+		boardService.insertBoard(board);
+		
+		rttr.addFlashAttribute("result", board.getBon());
+		
+		return "redirect:/board/list";
+	}
+	
+	@GetMapping("/selectBoard")
+	public void selectBoard(@RequestParam("bon") Long bon, Model model) {
+		log.info("/get");
+		model.addAttribute("board", boardService.selectBoard(bon));
+	}
+	
+	@PostMapping("/updateBoard")
+	public String updateBoard(BoardVO board, RedirectAttributes rttr) {
+		log.info("update : " + board);
+		
+		if (boardService.updateBoard(board)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/deleteBoard")
+	public String deleteBoard(@RequestParam("bon") Long bon, RedirectAttributes rttr) {
+		log.info("delete : " + bon);
+		
+		if (boardService.deleteBoard(bon)) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		return "redirect:/board/list";
+	}
+}
---
+++ ex02/src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -0,0 +1,6 @@
+package org.zerock.controller;
+
+// 굳이 해야될까라는 생각이 들어서 안할거임
+public class BoardControllerTests {
+
+}
